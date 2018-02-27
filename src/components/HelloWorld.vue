ff0a70caf148ba02ed7b58777b3e46597b31927c
w3slab <36891194+w3slab@users.noreply.github.com>
2018-02-27 16:39:25 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-radio v-model="radio" label="1" border>Option A</el-radio>
+    <el-radio v-model="radio" label="2" border>Option B</el-radio>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      radio: "1"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
